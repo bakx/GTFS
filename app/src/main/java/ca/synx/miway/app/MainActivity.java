2a72bd2aa9916d2b9ff2c25558568d24cb53f742
Gideon <gideon1982@gmail.com>
2014-03-21 22:42:49 -0400

    :)

--- app/src/main/java/ca/synx/miway/app/MainActivity.java
+++ app/src/main/java/ca/synx/miway/app/MainActivity.java
@@ -1,98 +1,37 @@
 package ca.synx.miway.app;
 
-import android.os.AsyncTask;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Xml;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.TabHost;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import ca.synx.miway.adapters.RouteAdapter;
 import ca.synx.miway.models.Route;
 
-// http://miway.dataservices.synx.ca/api/MiWay/GetRoutes
-// http://miway.dataservices.synx.ca/api/MiWay/GetStops/101/Eastbound
-// http://miway.dataservices.synx.ca/api/MiWay/GetStops/101/Eastbound/4300
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+    TabHost mTabHost;
+    ListView mRoutesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the action bar.
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // Init tabs
+        initializeTabs();
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        //
-        // Add main tabs.
-        //
-
-        for (int i = 0; i < mSectionsPagerAdapter.getApplicationTabs().length; i++) {
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getApplicationTabs()[i])
-                            .setTabListener(this)
-            );
-        }
+        loadRoutesTab();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -104,295 +43,49 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
+    // Load Tabs
+    protected void initializeTabs() {
+
+        mTabHost = (TabHost) findViewById(R.id.tabHost);
+        mTabHost.setup();
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab_favorites")
+                        .setContent(R.id.tab1)
+                        .setIndicator(getResources().getString(R.string.tab_favorites)
+                        )
+        );
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab_routes")
+                        .setContent(R.id.tab2)
+                        .setIndicator(getResources().getString(R.string.tab_routes)
+                        )
+        );
     }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    protected void loadRoutesTab() {
+        mRoutesListView = (ListView) findViewById(R.id.listView);
+
+        List<Route> routes = new ArrayList<Route>();
+
+        routes.add(new Route(1, 1, "1 Dundas", "Eastbound"));
+        routes.add(new Route(2, 1, "1 Dundas", "Westbound"));
+
+        routes.add(new Route(1, 10, "10 Bristol-Britannia", "Northbound"));
+        routes.add(new Route(2, 10, "10 Bristol-Britannia", "Southbound"));
+
+        routes.add(new Route(1, 10, "101 Dundas Express", "Eastbound"));
+        routes.add(new Route(2, 10, "101 Dundas Express", "Westbound"));
+
+        RouteAdapter adapter = new RouteAdapter(routes, this);
+        mRoutesListView.setAdapter(adapter);
     }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        public final String[] getApplicationTabs() {
-            return new String[]{
-                    getResources().getString(R.string.tab_favorites),
-                    getResources().getString(R.string.tab_routes),
-                    getResources().getString(R.string.tab_stops)
-            };
-        }
-
-        @Override
-        @Deprecated
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
-    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                return loadXmlFromNetwork(urls[0]);
-            } catch (IOException e) {
-                return getResources().getString(R.string.connection_error);
-            } catch (XmlPullParserException e) {
-                return getResources().getString(R.string.xml_error);
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            setContentView(R.layout.activity_main);
-
-            // Displays the HTML string in the UI via a WebView
-
-            TextView textView = (TextView) findViewById(R.id.section_label);
-            textView.setText(result);
-        }
-    }
-
-    // Uploads XML from stackoverflow.com, parses it, and combines it with
-    // HTML markup. Returns HTML string.
-    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
-        InputStream stream = null;
-        StackOverflowXmlParser stackOverflowXmlParser = new StackOverflowXmlParser();
-        List<Route> entries = null;
-//        Calendar rightNow = Calendar.getInstance();
-        //       DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
-
-        // Checks whether the user set the preference to include summary text
-        //SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //boolean pref = sharedPrefs.getBoolean("summaryPref", false);
-
-        StringBuilder htmlString = new StringBuilder();
-        //htmlString.append("<h3>" + getResources().getString(R.string.page_title) + "</h3>");
-        //htmlString.append("<em>" + getResources().getString(R.string.updated) + " " +
-        //        formatter.format(rightNow.getTime()) + "</em>");
-
-        try {
-            stream = downloadUrl(urlString);
-            entries = stackOverflowXmlParser.parse(stream);
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-        }
-
-        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
-        // Each Entry object represents a single post in the XML feed.
-        // This section processes the entries list to combine each entry with HTML markup.
-        // Each entry is displayed in the UI as a link that optionally includes
-        // a text summary.
-        for (Route route : entries) {
-            //htmlString.append("<p><a href='");
-            //htmlString.append(entry.link);
-            //htmlString.append("'>" + entry.title + "</a></p>");
-            // If the user set the preference to include summary text,
-            // adds it to the display.
-            //if (pref) {
-            //    htmlString.append(entry.summary);
-            // }
-        }
-        return htmlString.toString();
-    }
-
-    // Given a string representation of a URL, sets up a connection and gets
-    // an input stream.
-    private InputStream downloadUrl(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Starts the query
-        conn.connect();
-        InputStream stream = conn.getInputStream();
-        return stream;
-    }
-
-
-    public class StackOverflowXmlParser {
-        private String ns = null;
-
-        public List<Route> parse(InputStream in) throws XmlPullParserException, IOException {
-            try {
-                XmlPullParser parser = Xml.newPullParser();
-                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-                parser.setInput(in, null);
-                parser.nextTag();
-                return readFeed(parser);
-            } finally {
-                in.close();
-            }
-        }
-
-        private List<Route> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-            List<Route> entries = new ArrayList<Route>();
-
-            parser.require(XmlPullParser.START_TAG, ns, "feed");
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-                String name = parser.getName();
-                // Starts by looking for the entry tag
-                if (name.equals("entry")) {
-                    entries.add(readEntry(parser));
-                } else {
-                    skip(parser);
-                }
-            }
-            return entries;
-        }
-
-
-        // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them
-        // off
-        // to their respective &quot;read&quot; methods for processing. Otherwise, skips the tag.
-        private Route readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
-            parser.require(XmlPullParser.START_TAG, ns, "entry");
-            String title = null;
-            String summary = null;
-            String link = null;
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-                String name = parser.getName();
-                if (name.equals("title")) {
-                    title = readTitle(parser);
-                } else if (name.equals("link")) {
-                    link = readLink(parser);
-                } else {
-                    skip(parser);
-                }
-            }
-            return new Route(); //title, summary, link);
-        }
-
-        // Processes title tags in the feed.
-        private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
-            parser.require(XmlPullParser.START_TAG, ns, "title");
-            String title = readText(parser);
-            parser.require(XmlPullParser.END_TAG, ns, "title");
-            return title;
-        }
-
-        // Processes link tags in the feed.
-        private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
-            String link = "";
-            parser.require(XmlPullParser.START_TAG, ns, "link");
-            String tag = parser.getName();
-            String relType = parser.getAttributeValue(null, "rel");
-            if (tag.equals("link")) {
-                if (relType.equals("alternate")) {
-                    link = parser.getAttributeValue(null, "href");
-                    parser.nextTag();
-                }
-            }
-            parser.require(XmlPullParser.END_TAG, ns, "link");
-            return link;
-        }
-
-        // For the tags title and summary, extracts their text values.
-        private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
-            String result = "";
-            if (parser.next() == XmlPullParser.TEXT) {
-                result = parser.getText();
-                parser.nextTag();
-            }
-            return result;
-        }
-
-        // Skips tags the parser isn't interested in. Uses depth to handle nested tags. i.e.,
-        // if the next tag after a START_TAG isn't a matching END_TAG, it keeps going until it
-        // finds the matching END_TAG (as indicated by the value of "depth" being 0).
-        private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                throw new IllegalStateException();
-            }
-            int depth = 1;
-            while (depth != 0) {
-                switch (parser.next()) {
-                    case XmlPullParser.END_TAG:
-                        depth--;
-                        break;
-                    case XmlPullParser.START_TAG:
-                        depth++;
-                        break;
-                }
-            }
-        }
-
-    }
-
 }
--- app/src/main/java/ca/synx/miway/models/Route.java
+++ app/src/main/java/ca/synx/miway/models/Route.java
@@ -4,8 +4,15 @@ package ca.synx.miway.models;
  * Created by Gideon on 3/20/14.
  */
 public class Route {
-    public int routeId;
 
+    public Route(int routeId, int routeNumber, String routeName, String routeHeading) {
+        this.routeId = routeId;
+        this.routeNumber = routeNumber;
+        this.routeName = routeName;
+        this.routeHeading = routeHeading;
+    }
+
+    public int routeId;
     public int routeNumber;
     public String routeName;
     public String routeHeading;
